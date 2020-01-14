8cf96ba60b912f400ebafadc640b8338456b2578
hidde8erberg <hidde8erberg@gmail.com>
2020-01-14 15:33:22 +0100

    fixed board click

--- connectfour/public/javascripts/Board.js
+++ connectfour/public/javascripts/Board.js
@@ -10,17 +10,19 @@ function Board() {
         [0,0,0,0,0,0]
     ]
 
+
     this.place = function(col, player) {
         if (this.checkcol(col)) {
             return;
         }
 
         for (let i = 0; i < 6; i++) {
-            if (this.board[col][i] == this.empty) {
+            if (this.board[col][i] == 0) {
                 this.board[col][i] = player;
 
                 var square = document.getElementById("c"+col).children[5-i].children[0];
                 square.style.backgroundColor = "yellow";
+                console.log(1);
                 
                 break;
             }
@@ -29,7 +31,7 @@ function Board() {
     };
 
     this.checkcol = function(col) {
-        if (this.board[col][5] != this.empty) {
+        if (this.board[col][5] != 0) {
             return true;
         } else {
             return false;
